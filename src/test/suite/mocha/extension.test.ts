5202fb90dd8bf7faca7692e8b8c3e716220a5f09
Sarav <sarav.majestic@gmail.com>
2023-12-14 13:26:28 +0530

    fix: both mocha and jest together

--- src/test/suite/mocha/extension.test.ts
+++ src/test/suite/mocha/extension.test.ts
@@ -5,24 +5,24 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 import { Container } from "inversify";
-import TYPES from "../../types";
-import { CommandsManager } from "../../commands/commands-manager";
+import { CommandsManager } from "../../../commands/commands-manager";
+import TYPES from "../../../types";
 
-describe("Extension Test Suite", () => {
+suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
   let container: Container;
-  beforeAll(async () => {
+  setup("Load Extension", async () => {
     const ext = vscode.extensions.getExtension(
       "saravmajestic.vscode-inversify-jest"
     );
     container = await ext!.activate();
     const cmdManager = container.get<CommandsManager>(TYPES.CommandManager);
-    console.log("cmdManager in extension.test", cmdManager);
+    console.log("cmdManager in tests", cmdManager);
     console.log("extension activated");
   });
 
-  it("Sample test", () => {
+  test("Sample test", () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   });
